3e87c8fc3e0a41f619fe9f74e9e1d39ba4274335
patriciasukore <128140402+patriciasukore@users.noreply.github.com>
2024-04-15 22:21:37 +0100

    Add files via upload

---
+++ day1/day1.js
@@ -0,0 +1,17 @@
+let student1 = document.getElementById("student-1");
+'{'
+student1.addEventListener("click", () => {
+  student1.style.backgroundColor = "purple";
+});
+
+let student2 = document.getElementById("student-2");
+'{'
+student2.addEventListener("click", () => {
+  student2.style.backgroundColor = "green";
+});
+
+let student3 = document.getElementById("student-3");
+'{'
+student3.addEventListener("click", () => {
+  student3.style.backgroundColor = "green";
+});
